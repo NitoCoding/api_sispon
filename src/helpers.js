ce5d05ab4dbdb5122676b2eddac42ada70ce13ef
NitoCoding <ilhamhendra03@gmail.com>
2025-05-08 15:55:38 +0800

    feat(helpers): add QR code generation and HTML to PDF conversion functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,8 @@
 import crypto from 'crypto';
 import { config } from './config/index.js';
 import { AppError } from './middleware/errorHandler.js';
+import QRCode from 'qrcode';
+import wkhtmltopdf from 'wkhtmltopdf';
 
 const algorithm = 'aes-256-cbc';
 const secret = config.secretKey;
@@ -49,3 +51,47 @@ export const decrypt = (encryptedText) => {
         throw new AppError('Decryption failed: ' + error.message, 500);
     }
 };
+
+const generateQRCode = async (imagePath) => {
+    try {
+        // Baca file gambar sebagai base64
+        const imageBuffer = await fs.readFile(imagePath);
+        const base64Image = `data:image/png;base64,${imageBuffer.toString('base64')}`;
+
+        // Generate QR code dari data base64 gambar
+        const qrDataURL = await QRCode.toDataURL(base64Image);
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`<img src="${qrDataURL}" alt="QR Code TTD" />`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error generating QR code');
+    }
+}
+
+const htmlToPdf = async (templateReportName) => {
+
+    const templatePath = path.join(__dirname, 'views', 'templateReportName.ejs');
+    
+    try {
+        // Render HTML dari EJS template
+        const html = await ejs.renderFile(templatePath, { users });
+
+        // Set header untuk download sebagai PDF
+        res.header('Content-Type', 'application/pdf');
+        res.header('Content-Disposition', 'attachment; filename=Laporan_Pengguna.pdf');
+
+        // Konversi HTML ke PDF menggunakan wkhtmltopdf
+        wkhtmltopdf(html, {
+            output: null, // Output sebagai stream
+            pageSize: 'A4',
+            orientation: 'Portrait',
+            marginTop: '20mm',
+            marginBottom: '20mm',
+            marginLeft: '15mm',
+            marginRight: '15mm',
+        }).pipe(res);
+    } catch (error) {
+        
+    }
+}

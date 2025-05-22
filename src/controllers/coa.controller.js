5854c82f42ce146d11131d8e6a8b6a98e7dcdabe
ilham <ilhamhendra03@gmail.com>
2025-05-22 10:46:22 +0800

    refactor(coa.controller): add sorting to getCoa method
    Enhance the getCoa method by sorting the results by `kode` and `parent_id` in ascending order to ensure consistent and logical data presentation

--- src/controllers/coa.controller.js
+++ src/controllers/coa.controller.js
@@ -4,7 +4,12 @@ import { prisma } from "../prisma.js";
 export class CoaController {
     static getCoa = async (req, res, next) => {
         try {
-            const coa = await prisma.coa.findMany();
+            const coa = await prisma.coa.findMany({
+                orderBy: {
+                    kode: "asc",
+                    parent_id: "asc"
+                }
+            });
 
             return res.status(200).json({
                 success: true,
